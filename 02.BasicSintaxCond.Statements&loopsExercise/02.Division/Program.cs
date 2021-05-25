17a4152cd26f1b7298cddfefdd332a39d5df24c5
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-25 14:41:48 +0100

    Add files via upload

---
+++ 02.BasicSintaxCond.Statements&loopsExercise/02.Division/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02.Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 10 == 0)
+            {
+                Console.WriteLine("The number is divisible by 10");
+                return;
+            }
+            else if (num % 7 == 0)
+            {
+                Console.WriteLine("The number is divisible by 7");
+                return;
+            }
+            else if (num % 6 == 0)
+            {
+                Console.WriteLine("The number is divisible by 6");
+                return;
+            }
+            else if (num % 3 == 0)
+            {
+                Console.WriteLine("The number is divisible by 3");
+                return;
+            }
+            else if (num % 2 == 0)
+            {
+                Console.WriteLine("The number is divisible by 2");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Not divisible");
+            }
+
+        }
+    }
+}
