b6e04ca0cea1d08c4b8b7bfe49c8e175d3002a00
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-26 16:58:45 +0100

    Add files via upload

---
+++ 02.BasicSintaxCond.Statements&loopsExercise/06.StrongNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06.StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int numCopy = num;
+            int sum = 0;
+
+            while (num != 0)
+            {
+                int factoriel = 1;
+                int number = num % 10;
+                num /= 10;
+                for (int i = 2; i <= number; i++)
+                {
+                    factoriel *= i;
+                }
+                sum += factoriel;
+            }
+            if (sum == numCopy)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
