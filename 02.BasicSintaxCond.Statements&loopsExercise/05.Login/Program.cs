c51a31f03bfb698bebc8a437e164bdeeccb92468
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-26 16:58:21 +0100

    Add files via upload

---
+++ 02.BasicSintaxCond.Statements&loopsExercise/05.Login/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05.Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            string command = Console.ReadLine();
+            int counter = 1;
+            while (command != password)
+            {
+                string secondCommand = command;
+                if (counter != 4)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"User {username} logged in.");
+        }
+    }
+}
