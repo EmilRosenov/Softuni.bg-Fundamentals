8978e971420b647f7b1c95f8bb72c8b096360b19
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-26 16:59:18 +0100

    Add files via upload

---
+++ 02.BasicSintaxCond.Statements&loopsExercise/07.TriangleOfNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07.TriangleOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int start = 1;
+
+            for (int rows = 1; rows <= num; rows++)
+            {
+
+                for (int cows = 1; cows <= start; cows++)
+                {
+                    Console.Write(start + " ");
+
+                }
+                Console.WriteLine();
+                start++;
+            }
+        }
+    }
+}
