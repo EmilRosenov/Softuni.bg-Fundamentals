513c54f4e39cd9ddc441f7076e51c944d25b93d2
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-27 20:58:02 +0100

    Add files via upload

---
+++ 03.DataTypesAndVariables - Lab/02.ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02.ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0m;
+            for (int i = 0; i < n; i++)
+            {
+                decimal num = decimal.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
