d8b6a39d5b5cce9e09e77d910564dcd2f0715770
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-27 20:58:22 +0100

    Add files via upload

---
+++ 03.DataTypesAndVariables - Lab/03.CenturiesToMinutes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03.CenturiesToMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422m);
+            int hours = days * 24;
+            int minutes = hours * 60;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
