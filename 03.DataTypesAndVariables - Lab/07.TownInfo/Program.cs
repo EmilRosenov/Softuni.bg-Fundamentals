7a3b01833d3053bdb92d6bb0153b3c5a82f21d0c
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-28 16:30:44 +0100

    Add files via upload

---
+++ 03.DataTypesAndVariables - Lab/07.TownInfo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07.TownInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            uint population = uint.Parse(Console.ReadLine());
+            uint area = uint.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {name} has population of {population} and area {area} square km.");
+        }
+    }
+}
