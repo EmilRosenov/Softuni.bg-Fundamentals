a6ac49e57f4238874ac8330f49450fdeff381f22
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-28 16:29:40 +0100

    Add files via upload

---
+++ 03.DataTypesAndVariables - Lab/04.SpecialNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int num = i;
+                int sum = 0;
+                while (num > 0)
+                {
+                    int lastDigit = num % 10;
+                    sum += lastDigit;
+                    num /= 10;
+                }
+                bool specialOrNot = sum == 5 || sum == 7 || sum == 11;
+                Console.WriteLine($"{i} -> {specialOrNot}");
+            }
+        }
+    }
+}
