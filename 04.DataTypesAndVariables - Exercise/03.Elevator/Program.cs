fe2f0b9071af0bc9ccfd5c82057462163845caf8
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-29 20:22:31 +0100

    Add files via upload

---
+++ 04.DataTypesAndVariables - Exercise/03.Elevator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPeople = int.Parse(Console.ReadLine());
+            int capacityOfElevator = int.Parse(Console.ReadLine());
+            int courses = 0;
+            if (numOfPeople % capacityOfElevator == 0)
+            {
+                courses = (numOfPeople / capacityOfElevator);
+            }
+            else
+            {
+                courses = (numOfPeople / capacityOfElevator) + 1;
+            }
+            Console.WriteLine(courses);
+        }
+    }
+}
