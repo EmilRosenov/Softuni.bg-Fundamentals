a4f7148d1c6b8c9966ac3ae2db5c7a60a622ec50
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-30 20:42:47 +0100

    Add files via upload

---
+++ 04.DataTypesAndVariables - Exercise/05.PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05.PrintPartOfASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            for (int i = start; i <= end; i++)
+            {
+                char symbol = (char)i;
+                Console.Write($"{symbol} ");
+            }
+
+        }
+    }
+}
