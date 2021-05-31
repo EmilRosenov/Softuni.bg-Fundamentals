00e23dfc98735a460b1a717fdc8222c071f53b4e
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-05-31 23:34:55 +0100

    Add files via upload

---
+++ 04.DataTypesAndVariables - Exercise/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06.TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (char i = 'a'; i < 'a' + n; i++)
+            {
+                for (char f = 'a'; f < 'a' + n; f++)
+                {
+                    for (char j = 'a'; j < 'a' + n; j++)
+                    {
+                        Console.WriteLine($"{i}{f}{j}");
+                    }
+                }
+            }
+        }
+    }
+}
