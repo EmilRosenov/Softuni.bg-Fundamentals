642ca16b8349e4a888224e14023531117e2866bf
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-01 20:04:20 +0100

    Add files via upload

---
+++ 04.DataTypesAndVariables - Exercise/07.WaterOverflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07.WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int litersInTank = 0;
+            for (int i = 0; i < lines; i++)
+            {
+                int litersToAdd = int.Parse(Console.ReadLine());
+                if (litersInTank + litersToAdd <= 255)
+                {
+                    litersInTank += litersToAdd;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine($"{litersInTank}");
+        }
+    }
+}
