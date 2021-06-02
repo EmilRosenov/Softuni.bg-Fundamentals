ea41bb39d92813a762b251387b5598b9375e39c8
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-02 18:26:19 +0100

    Add files via upload

---
+++ 04.DataTypesAndVariables - Exercise/08.BeerKegs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08.BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string nameOfBiggestKeg = String.Empty;
+            double maxVolume = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string nameOfKeg = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                double volume = Math.PI * radius * radius * height;
+                if (volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    nameOfBiggestKeg = nameOfKeg;
+                }
+            }
+            Console.WriteLine(nameOfBiggestKeg);
+        }
+    }
+}
