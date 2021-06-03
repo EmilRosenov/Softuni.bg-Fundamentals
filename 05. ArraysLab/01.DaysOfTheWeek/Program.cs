bf68d9a41639df6e3f85f1d8828906a071344706
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-03 19:28:04 +0100

    Add files via upload

---
+++ 05. ArraysLab/01.DaysOfTheWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace daysOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays =   { "Monday",
+                                    "Tuesday",
+                                    "Wednesday",
+                                    "Thursday",
+                                    "Friday",
+                                    "Saturday",
+                                    "Sunday" };
+
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= 1 && number <= 7)
+            {
+                Console.WriteLine(weekDays[number - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+                
+            }
+
+        }
+    }
+}
