b7f5f86165751d1204411befecd39e07a7da6b3b
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-06 21:06:17 +0100

    Add files via upload

---
+++ 05. ArraysLab/04.ReverseString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ",Console.ReadLine()
+                .Split()
+                .Reverse())) ;
+
+            //Greshno Uprajnenie po-dolu;
+
+            //string line = Console.ReadLine();
+            //string[] newLine = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            //
+            //for (int i = 0; i < line.Length; i++)
+            //{
+            //    
+            //    
+            //}
+            //newLine[i] = string.Join(" ", line.Reverse());
+            //Console.WriteLine(newLine);
+
+        }
+    }
+}
