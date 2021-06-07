72806e9ccf4bf598d34ee0035c63ead3b6a78210
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-07 21:48:52 +0100

    Add files via upload

---
+++ 05. ArraysLab/05.EvenElementSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace evenElementSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] numbers = Console.ReadLine()
+                            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
