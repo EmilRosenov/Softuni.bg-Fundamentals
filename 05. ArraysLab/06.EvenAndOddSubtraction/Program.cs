defccadf141990cc36f855854e05bd963069c430
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-08 22:11:01 +0100

    Add files via upload

---
+++ 05. ArraysLab/06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _6EvenAndOddSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int oddSum = 0;
+            int evenSum = 0;
+            int difference = 0;
+            int[] numbers = Console.ReadLine()
+                            .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+                else
+                {
+                    oddSum += numbers[i];
+                }
+            }
+
+            difference = evenSum - oddSum;
+            Console.WriteLine($"{difference}");
+            
+
+        }
+    }
+}
