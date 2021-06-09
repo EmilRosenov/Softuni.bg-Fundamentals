4d00a5237ce311fb7094b0d911e46ed693c1e67b
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-09 22:56:34 +0100

    Add files via upload

---
+++ 05. ArraysLab/07.EqualArrays100ot100/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _7.equalArrays100ot100prepisReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] firstArr = Console.ReadLine()
+                             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                             .Select(int.Parse)
+                             .ToArray();
+
+            int[] secondArr = Console.ReadLine()
+                             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                             .Select(int.Parse)
+                             .ToArray();
+
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                if (firstArr[i] != secondArr[i])
+
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+                
+            }
+            sum += firstArr.Sum();
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+
+        }
+    }
+}
