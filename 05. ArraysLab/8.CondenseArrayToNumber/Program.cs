fafb438e1c3a409990b7395743fc96eb6f6c7142
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-10 17:16:26 +0100

    Add files via upload

---
+++ 05. ArraysLab/8.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _8.CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] firstArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            if (firstArray.Length == 1)
+            {
+                Console.WriteLine(firstArray[0]);
+                return;
+            }
+
+            else
+            {
+               
+                while (firstArray.Length != 1) // did it with else if()before 
+                {
+                    int[] secondArray = new int[firstArray.Length - 1]; //was on top of while before...
+                    for (int i = 0; i < firstArray.Length - 1; i++)
+                    {
+                        secondArray[i] = firstArray[i] + firstArray[i + 1];
+
+                    }
+                    firstArray = secondArray;
+
+                }
+                Console.WriteLine(firstArray[0]);  //thats why here the result was 12...  0 + 1;
+            }
+
+            
+
+        }
+    }
+}
