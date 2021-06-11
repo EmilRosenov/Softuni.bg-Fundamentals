104d748b326aaebb6e5204ce43695948be0b1b2c
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-11 16:59:05 +0100

    Add files via upload

---
+++ 06.ArraysExercise/001.AgainExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _001.AgainExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
