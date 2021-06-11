aefc4d540af5645949003dd034f455c0c57613a3
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-11 16:59:51 +0100

    Add files via upload

---
+++ 06.ArraysExercise/01.Train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace zad1.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] wagons = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                wagons[i] = int.Parse(Console.ReadLine());
+                sum += wagons[i];
+
+            }
+            Console.WriteLine(string.Join(" ",wagons));
+            Console.WriteLine(sum);
+        }
+    }
+}
