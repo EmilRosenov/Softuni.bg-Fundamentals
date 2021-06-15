5e086c81cbf7147b862a608e144ed65bf69ebdf6
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-15 18:36:03 +0100

    Add files via upload

---
+++ 06.ArraysExercise/04.ArrayRotation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace zad4.ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split().ToArray();
+
+            int numberOfRotations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfRotations; i++)
+            {
+                string firstElement = array[0];
+
+                for (int j = 1; j < array.Length; j++)
+                {
+                    string currentElement = array[j];
+                    array[j - 1] = currentElement;
+                }
+                array[array.Length - 1] = firstElement;
+            }
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
