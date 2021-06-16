920f46c888155404ec1881a4d5df16727656d209
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-16 18:29:33 +0100

    Add files via upload

---
+++ 06.ArraysExercise/05.TopIntegers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace zad5.TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                                 .Split(" ")
+                                 .Select(int.Parse)
+                                 .ToArray();
+            bool isBigger = true;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] <= array[j])
+                    {
+                        isBigger = false;
+                        break;
+                    }
+
+                }
+                if (isBigger)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                isBigger = true;
+            }
+
+        }
+    }
+}
