2fece9a82b3717a82d34cfb0a04442a6407d90f3
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-17 20:13:22 +0100

    Add files via upload

---
+++ 06.ArraysExercise/06.EqualSums/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace zad6.EqualSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                                 .Split()
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            //1 2 3 3   - output   2
+            //10 5 5 99 3 4 2 5 1 1 4   -- 3
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                int sumLeft = 0;
+                int sumRight = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    sumLeft += array[j];
+                }
+                for (int k = i+1; k < array.Length; k++)
+                {
+                    sumRight += array[k];
+
+                    
+                }
+
+                if (sumLeft == sumRight)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
