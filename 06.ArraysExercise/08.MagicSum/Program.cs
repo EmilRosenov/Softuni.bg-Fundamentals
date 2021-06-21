7bcf56b7bb8c6d37c94539a42de51fb79c3b5f37
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-06-21 23:10:28 +0100

    Add files via upload

---
+++ 06.ArraysExercise/08.MagicSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace zad8.MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int firstElement = array[i];
+
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    int secondElement = array[j];
+
+                    if (firstElement + secondElement == number)
+                    {
+                        Console.WriteLine($"{firstElement} {secondElement}");
+                    }
+                }
+            }
+            
+
+
+
+        }
+    }
+}
