00fef5ec888e513907713b41947089473a5c4490
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-05 11:17:44 +0100

    Add files via upload

---
+++ 07.MethodsLab/01.SignOfIntegerNumber/SignOfIntegerNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SignOfIntegerNumber
+{
+    class SignOfIntegerNumber
+    {
+        static void Main(string[] args)
+        {
+           
+
+            PrintNumber(int.Parse(Console.ReadLine()));
+        }
+
+        static void PrintNumber(int number)
+        {
+            if (number>0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number<0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
