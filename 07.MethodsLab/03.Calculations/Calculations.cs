5dd1da824bdbff56681c4135d6af5f0284dbcf3e
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-07 16:27:21 +0100

    Add files via upload

---
+++ 07.MethodsLab/03.Calculations/Calculations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Calculations
+{
+    class Calculations
+    {
+        static void Main(string[] args)
+        {
+            string action = Console.ReadLine(); //add, multiply, subtract, divide
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            PrintCalculationsResult(action, firstNumber, secondNumber);
+
+
+        }
+
+        static void PrintCalculationsResult(string input, double number, double anotherNumber)
+        {
+            double result = 0;
+
+            if (input == "add")
+            {
+                result = number + anotherNumber;
+            }
+            if (input == "multiply")
+            {
+                result = number * anotherNumber;
+            }
+            if (input == "subtract")
+            {
+                result = number - anotherNumber;
+            }
+            if (input == "divide")
+            {
+                result = number / anotherNumber;
+            }
+
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
