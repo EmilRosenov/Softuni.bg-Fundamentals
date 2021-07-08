ad8cfdf1a3efa4b1f988d045854ee81b2eb63e3e
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-08 04:07:41 +0100

    Add files via upload

---
+++ 07.MethodsLab/04.PrintTriangle/PrintTriangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PrintTriangle
+{
+    class PrintTriangle
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTheShape(n);
+        }
+
+
+        static void PrintnumbersFrom1(int to)
+        {
+            for (int i = 1; i <= to; i++)
+            {
+                Console.Write(i+" ");
+            }
+        }
+
+        static void PrintTheShape(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                PrintnumbersFrom1(i);
+                Console.WriteLine();
+
+            }
+
+            for (int i = number - 1; i > 0; i--)
+            {
+                PrintnumbersFrom1(i);
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+    
+}
