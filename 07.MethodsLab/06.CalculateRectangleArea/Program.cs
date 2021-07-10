6fc7b9e31b91f021afa1928c6a3ebc17e3614436
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-10 22:14:51 +0100

    Add files via upload

---
+++ 07.MethodsLab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06_Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = GetSqureArea(width, height);
+            Console.WriteLine(area);
+        }
+        private static double GetSqureArea(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
