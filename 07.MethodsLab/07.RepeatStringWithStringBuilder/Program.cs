b6ed656f8cff185b3c92e9f3e6f0e8bb7634b409
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-11 09:50:27 +0100

    Add files via upload

---
+++ 07.MethodsLab/07.RepeatStringWithStringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatStringWithStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int timesToRepeat = int.Parse(Console.ReadLine());
+
+            string result = PrintInput(input, timesToRepeat);
+            Console.WriteLine(result);
+        }
+
+        static string PrintInput(string something, int repeat)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < repeat; i++)
+            {
+                result.Append(something);
+                
+            }
+            return result.ToString();
+        }
+    }
+}
