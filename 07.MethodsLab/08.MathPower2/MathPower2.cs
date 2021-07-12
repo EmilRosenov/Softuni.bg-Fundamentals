e7ab324de4cf6eba3abbef63d3be0f1b23c61ff9
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-12 13:57:47 +0100

    Add files via upload

---
+++ 07.MethodsLab/08.MathPower2/MathPower2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MathPower2
+{
+    class MathPower2
+    {
+        static void Main(string[] args)
+        { 
+
+            Console.WriteLine(PrintResult(double.Parse(Console.ReadLine()),
+                        double.Parse(Console.ReadLine())));
+        }
+
+        static double PrintResult(double x, double y)
+        {
+            double result = Math.Pow(x, y);
+            return result;
+        }
+    }
+}
