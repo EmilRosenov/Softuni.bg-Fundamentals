11ec1496ac2cb12a66b739c0a1ea2217477fefef
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-14 13:30:57 +0100

    Add files via upload

---
+++ 07.MethodsLab/10.MultiplyEvensByOdds/MultiplyEvensByOdds.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MultiplyEvensByOdds
+{
+    class MultiplyEvensByOdds
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+
+        }
+
+        static int GetMultipleOfEvenAndOdds(int number)
+        { 
+            int result = GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+            return result;
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sumEven = 0;
+
+            while(number > 0)
+            {
+                int lastnumber = number % 10;
+
+                if(lastnumber % 2 ==0)
+                {
+                    sumEven += lastnumber;
+                }
+
+                number = number / 10;
+            }
+            return sumEven;
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            int sumOdd = 0;
+
+            while (number > 0)
+            {
+                int lastnumber = number % 10;
+
+                if (lastnumber % 2 != 0)
+                {
+                    sumOdd += lastnumber;
+                }
+
+                number = number / 10;
+            }
+            return sumOdd;
+        }
+    }
+}
