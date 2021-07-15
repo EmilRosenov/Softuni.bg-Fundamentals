3f679a6545d5e387fc2fc03fde0e614f0e709335
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-15 20:24:27 +0100

    Add files via upload

---
+++ 07.MethodsLab/11.MathOperations/MathOperations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MathOperations
+{
+    class MathOperations
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            string action = Console.ReadLine();
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(PrintResult(x, action, y));
+        }
+
+        static double PrintResult(double x, string action, double y)
+        {
+            double result = 0;
+            if (action=="/")
+            {
+                result = x / y;
+            }
+            else if (action == "*")
+            {
+                result = x * y;
+            }
+            else if (action == "+")
+            {
+                result = x + y;
+            }
+            else if (action == "-")
+            {
+                result = x - y;
+            }
+
+            return result;
+        }
+
+    }
+}
