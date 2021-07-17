6e6356c30e9a7be13d3febe4a3c0c54527bb8001
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-17 09:44:28 +0100

    Add files via upload

---
+++ 08.MethodsExercise/2.Characters in Range/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _03Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            if (secondChar > firstChar)
+            {
+                PrintInBetweenChars(secondChar, firstChar);
+            }
+            else
+            {
+                PrintReversed(firstChar, secondChar);
+            }
+            
+
+        }
+
+        private static void PrintReversed(char firstChar, char secondChar)
+        {
+            char first = firstChar;
+            for (int i = secondChar + 1 ; i < firstChar; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+
+        private static void PrintInBetweenChars(char firstChar, char secondChar)
+        {
+            //char first = firstChar;
+            for (int i = secondChar + 1 ; i < firstChar; i++)
+            {
+                Console.Write((char)i + " ");
+            } 
+        }
+    }
+}
