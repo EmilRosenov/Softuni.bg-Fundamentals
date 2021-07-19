f787310c2bc4178049bda0d2a9bc233e8a6d34c3
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-19 11:53:10 +0100

    Add files via upload

---
+++ 08.MethodsExercise/4.AddAndSubtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int sum = SumFirstTwo(firstNumber, secondNumber);
+            int result = SubtractSumFirstTwoAndThirdNumber(sum, thirdNumber);
+            Console.WriteLine(result);
+        }
+
+        private static int SubtractSumFirstTwoAndThirdNumber(int sum, int thirdNumber)
+        {
+            return sum - thirdNumber;
+        }
+
+        private static int SumFirstTwo(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
