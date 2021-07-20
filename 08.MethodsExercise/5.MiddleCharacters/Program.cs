23bc61eec69cfc3776d0937794836456145c5f63
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-20 17:09:29 +0100

    Add files via upload

---
+++ 08.MethodsExercise/5.MiddleCharacters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input.Length % 2 == 0)
+            {
+                string output = GetMiddleFromAnEvenINput(input);
+                Console.WriteLine(output);
+            }
+            else
+            {
+                string outputFromOdd = GetMiddleFromOddInput(input);
+                Console.WriteLine(outputFromOdd);
+            }
+        }
+
+        private static string GetMiddleFromOddInput(string input)
+        {
+            int index = input.Length / 2;
+            string chars = input.Substring(index, 1);
+            return chars;
+        }
+
+        private static string GetMiddleFromAnEvenINput(string input)
+        {
+            int index = input.Length / 2;
+            string chars = input.Substring(index - 1, 2);
+            return chars;
+        }
+    }
+}
