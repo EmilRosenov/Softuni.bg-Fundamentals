a3496e9c1dd208eea9c17e13b92479d00a322451
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-21 15:55:37 +0100

    Add files via upload

---
+++ 08.MethodsExercise/6.NxNMatrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintMatrix(n);
+
+        }
+
+        private static void PrintMatrix(int n)
+        {
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int columns = 1; columns <= n; columns++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
