28b4b524e7333ab55500f557df94f215c5472b44
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-22 13:57:36 +0100

    Add files via upload

---
+++ 08.MethodsExercise/7.FactorialDivision/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstInteger = int.Parse(Console.ReadLine());
+            int secondInteger =int.Parse(Console.ReadLine());
+
+            double factorialFirstInt = GetFactorial(firstInteger);
+            double factorialSecondInt = GetFactorial(secondInteger);
+            double sum = factorialFirstInt / factorialSecondInt;
+            Console.WriteLine($"{sum:f2}");
+        }
+
+        private static double GetFactorial(int number)
+        {
+           
+                double result = 1;
+                while (number != 1)
+                {
+                    result = result * number;
+                    number = number - 1;
+                }
+                return result;
+        }
+    }
+}
+
+
