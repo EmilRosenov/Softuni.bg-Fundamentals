8a3cc13f4291e2a500301e17793478509b7380e4
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-27 13:44:17 +0100

    Add files via upload

---
+++ 09.ListsLab/02.GausTrick/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GausTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first + last, first + 1 + last - 1, first + 2 + last - 2,
+            // … first + n, last - n.
+
+            List<int> numbers = Console.ReadLine()
+                                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                        .Select(int.Parse)
+                                        .ToList();
+
+            //Console.WriteLine("Here it is");
+            //Console.WriteLine(string.Join(" ",numbers));
+
+            int originalCount = numbers.Count;
+            for (int i = 0; i < originalCount / 2; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.Remove(numbers[numbers.Count - 1]);
+
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+            
+
+        }
+    }
+}
