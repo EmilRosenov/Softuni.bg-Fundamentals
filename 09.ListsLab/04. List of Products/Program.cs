aa69d232411d57a34b2b9e8acbeb19694063e071
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-07-29 15:28:04 +0100

    Add files via upload

---
+++ 09.ListsLab/04. List of Products/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> vegetables = new List<string> (n);
+
+            for (int i = 0; i < n; i++)
+            {
+                string currenetProduct = Console.ReadLine();
+                vegetables.Add(currenetProduct);
+                
+            }
+
+            vegetables.Sort();
+
+            for (int i = 0; i < vegetables.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{vegetables[i]}");
+            }
+            
+
+
+        }
+    }
+}
