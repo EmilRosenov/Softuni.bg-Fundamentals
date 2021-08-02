66657643947d2a46d9788f4207d502f1dc69742b
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-08-02 23:04:39 +0100

    Add files via upload

---
+++ 10.ListsExercise/01.ChangeList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+            string[] command = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            while (command[0].ToUpper() !="END")
+            {
+                switch (command[0].ToUpper())
+                {
+                    case "DELETE":
+                        myList.RemoveAll(n => n == int.Parse(command[1]));
+                        break;
+                    case "INSERT":
+                        myList.Insert(int.Parse(command[2]),
+                            int.Parse(command[1]));
+                        break;
+                    default:
+                        break;
+                }
+
+
+                command = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+
+            Console.WriteLine(string.Join(' ', myList));
+        }
+    }
+}
