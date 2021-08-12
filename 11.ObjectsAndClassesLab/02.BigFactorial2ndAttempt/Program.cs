c5eb88451cf82a8963b30741d8398f546407bfd5
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-08-12 13:16:33 +0100

    Add files via upload

---
+++ 11.ObjectsAndClassesLab/02.BigFactorial2ndAttempt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigFactorial2ndAttempt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial.Calculate(number));
+            
+        }
+    }
+
+    public class Factorial
+    {
+        public static BigInteger Calculate(int n)
+        {
+            BigInteger result = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
