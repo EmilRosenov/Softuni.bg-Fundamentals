1861fb420409bc5265f140999a501d025ab1ef3d
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-08-20 09:59:59 +0100

    Add files via upload

---
+++ 12.ObjectsAndClassesExercise/03.Articles2.0/Articles2.0.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Articles2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            
+
+            List<Article> listOfArticles = new List<Article>();
+
+            for (int i = 0; i < number; i++)
+            {
+                string[] input = Console.ReadLine()
+                                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                    .ToArray();
+                string title = input[0];
+                string content = input[1];
+                string author = input[2];
+
+                Article newArticle = new Article(title, content, author);
+                listOfArticles.Add(newArticle);
+            }
+
+            string givenCriteria = Console.ReadLine();
+
+            if (givenCriteria == "title")
+            {
+                listOfArticles = listOfArticles.OrderBy(x => x.Title).ToList();
+                Console.WriteLine(string.Join(Environment.NewLine, listOfArticles));
+            }
+            else if (givenCriteria == "content")
+            {
+                listOfArticles = listOfArticles.OrderBy(x => x.Content).ToList();
+                Console.WriteLine(string.Join(Environment.NewLine, listOfArticles));
+            }
+            else if (givenCriteria == "author")
+            {
+                listOfArticles = listOfArticles.OrderBy(x => x.Author).ToList();
+                Console.WriteLine(string.Join(Environment.NewLine, listOfArticles));
+            }
+        }
+
+
+
+       public class Article
+        {
+            public string Title;
+            public string Content;
+            public string Author;
+
+            public Article(string title, string content, string author)
+            {
+                this.Title = title;
+                this.Content = content;
+                this.Author = author;
+
+               
+            }
+
+           public override string ToString()
+           {
+               return $"{Title} - {Content}: {Author}";
+           }
+        }
+    }
+}
