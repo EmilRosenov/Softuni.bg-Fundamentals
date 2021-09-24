fe31066f0e442cd6c03225b3701d4dfcd8807d69
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-09-24 09:43:47 +0100

    Add files via upload

---
+++ 13.AssociativeArraysLab/01.CountRealNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split()
+                            .Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> dictionary = new SortedDictionary<double, int>();
+
+            foreach (var item in input)
+            {
+                if (dictionary.ContainsKey(item))
+                {
+                    dictionary[item]++;
+                }
+                else
+                {
+                    dictionary.Add(item, 1);
+                }
+            }
+
+            foreach (var pairs in dictionary)
+            {
+                Console.WriteLine(pairs.Key + " -> " + pairs.Value);
+            }
+
+
+        }
+    }
+}
