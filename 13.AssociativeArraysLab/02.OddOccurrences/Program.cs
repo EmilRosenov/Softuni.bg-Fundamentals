4b43a5beeb6b670612241f97848b0277fcc6c39c
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-09-25 12:03:28 +0100

    Add files via upload

---
+++ 13.AssociativeArraysLab/02.OddOccurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (string word in input)
+            {
+                string wordInLowerCase = word.ToLower();
+                if (dict.ContainsKey(wordInLowerCase))
+                {
+                    dict[wordInLowerCase]++;
+                }
+                else
+                {
+                    dict.Add(wordInLowerCase, 1);
+                }
+            }
+            foreach (var words in dict)//.OrderByDescending(x=>x.Key))
+            {
+                if (words.Value % 2 != 0)
+                {
+                    Console.Write(words.Key + " ");
+                }
+                
+            }
+        }
+    }
+}
