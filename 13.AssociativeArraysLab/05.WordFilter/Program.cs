015afde89fa724c718533b16519af02c512090ec
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-09-28 14:55:19 +0100

    Add files via upload

---
+++ 13.AssociativeArraysLab/05.WordFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+
+            List<string> wordsList = new List<string>();
+
+            wordsList = words.Where(w => w.Length % 2 == 0).ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, wordsList));
+
+        }
+    }
+}
