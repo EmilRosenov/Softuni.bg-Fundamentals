18863789a4c2dbd3e05b0bf9ae0817fb9a7a70aa
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-09-29 12:06:16 +0100

    Add files via upload

---
+++ 14.AssociativeArraysExercise/01.CountCharsInAString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExerciseAssociativeArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            Dictionary<char, int> text = new Dictionary<char, int>();
+
+            foreach (var letter in input)
+            {
+                if (letter != ' ')
+                {
+                    if (text.ContainsKey(letter))
+                    {
+                        text[letter]++;
+                    }
+                    else
+                    {
+                        text.Add(letter, 1);
+                    }
+                }
+            }
+
+            foreach (var currentChar in text)
+            {
+                Console.WriteLine($"{currentChar.Key} -> {currentChar.Value}");
+            }
+        }
+    }
+}
