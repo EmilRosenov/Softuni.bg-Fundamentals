1cc5035170be3cdf7acdfdfeb805b44172850045
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-09-30 10:01:27 +0100

    Add files via upload

---
+++ 14.AssociativeArraysExercise/02.AMinersTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.AMinersTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, int> resourceAndQuantity = new Dictionary<string, int>();
+           
+            while (input!="stop")
+            {
+                    int quantity = int.Parse(Console.ReadLine());
+
+                        if (!resourceAndQuantity.ContainsKey(input))
+                        {
+                            resourceAndQuantity.Add(input, quantity);
+                        }
+                        else
+                        {
+                            resourceAndQuantity[input] += quantity;
+                        }
+
+                    input = Console.ReadLine();
+               
+            }
+
+            foreach (var kvp in resourceAndQuantity)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+           
+        }
+    }
+}
