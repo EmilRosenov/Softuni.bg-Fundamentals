fca3c31a81d9a67886af074c5b3b40a6a36179d4
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-08 13:43:29 +0100

    Add files via upload

---
+++ 15.TextProcessingLab/05.Digits,LettersAndOther/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05.Digits_LettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string digits = string.Empty;
+            string letters = string.Empty;
+            string others = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    digits += input[i];
+                }
+                else if (char.IsLetter(input[i]))
+                {
+                    letters += input[i];
+                }
+                else
+                {
+                    others += input[i];
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
