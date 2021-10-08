1e59fdcb96074f6dd651bb6c7311082292e1f886
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-08 13:42:06 +0100

    Add files via upload

---
+++ 15.TextProcessingLab/03.Substring/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03.Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToBeRemoved = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int indexOfWordToBeRemoved = text.IndexOf(wordToBeRemoved);
+
+            while (indexOfWordToBeRemoved != -1)
+            {
+                text = text.Remove(indexOfWordToBeRemoved, wordToBeRemoved.Length);
+
+                indexOfWordToBeRemoved = text.IndexOf(wordToBeRemoved);
+            }
+
+
+            Console.WriteLine(text);
+        }
+    }
+}
