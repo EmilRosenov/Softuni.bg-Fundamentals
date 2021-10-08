d0433098ccd76cd7fbd9669c9d9eb7dfc1441329
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-08 13:40:17 +0100

    Add files via upload

---
+++ 15.TextProcessingLab/01.ReverseStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                char[] result = input.Reverse().ToArray();
+                Console.WriteLine($"{input} = {string.Join("",result)}");
+            }
+        }
+    }
+}
