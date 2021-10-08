8131f91c96c4c79ed38b3a650e141a2cb5ef5e32
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-08 13:41:25 +0100

    Add files via upload

---
+++ 15.TextProcessingLab/02.RepeatString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                           .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string result = string.Empty;
+
+            foreach (string word in arr)
+            {
+                int wordLenght = word.Length;
+
+                for (int i = 0; i < wordLenght; i++)
+                {
+                    result += word;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
