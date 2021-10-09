205bd9740bb43a6438cfd1d03aa18fa8c5f19ebc
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-09 13:45:18 +0100

    Add files via upload

---
+++ 16.TextProcessingExercise/01.ValidUsername/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ValidUsername
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                var current = usernames[i];
+
+                if (IsValid(current))
+                {
+                    Console.WriteLine(current);
+                }
+            }
+        }
+
+        public static bool IsValid(string username)
+        {
+
+            return (username.Length >= 3 && username.Length <= 16)
+                   && (username.All(c => char.IsLetterOrDigit(c))
+                   || (username.Contains("-") || username.Contains("_")));
+        }
+
+    }
+}
