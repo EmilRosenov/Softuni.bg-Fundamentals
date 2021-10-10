5de428ed677f0f1d7f8cbe402e4e6d1069b8f701
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-10 20:05:54 +0100

    Add files via upload

---
+++ 16.TextProcessingExercise/02.CharacterMultiplier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _02.CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            string stringOne = input[0];
+            string stringTwo = input[1];
+
+            Console.WriteLine(Printsum(stringOne, stringTwo));
+
+            //Printsum();
+        }
+
+        public static int Printsum(string first, string second)
+        {
+            int totalSum = 0;
+            bool AreNotEqual = false;
+
+            while (first.Length == second.Length)
+            {
+                for (int i = 0; i < first.Length; i++)
+                {
+                   int sum =  first[i] * second[i];
+                   totalSum += sum;
+
+                    if (i >= first.Length-1)
+                    {
+                        AreNotEqual = true;
+                        break;
+                    }
+                }
+
+                if (AreNotEqual)
+                {
+                    break;
+                }
+            }
+
+
+
+            return totalSum;
+        }
+    }
+}
