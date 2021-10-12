223c2fb43051df090f0ba12aef9189629d313571
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-12 11:12:38 +0100

    Add files via upload

---
+++ 16.TextProcessingExercise/04.CaesarCipher/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            input = input.All(c => char.GetNumericValue(c) + 3).ToString();
+            
+        }
+    }
+}
