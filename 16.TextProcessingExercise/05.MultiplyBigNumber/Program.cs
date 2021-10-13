39de6efe8a0305242cf003fe38ad4d7e07ebf7a2
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-13 18:17:58 +0100

    Add files via upload

---
+++ 16.TextProcessingExercise/05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05.MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bigNumber = int.Parse(Console.ReadLine());
+            int otherNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(MultiplyBigNumber(bigNumber, otherNumber));
+        }
+
+        static int MultiplyBigNumber(int numberOne, int numberTwo)
+        {
+            int result = numberOne * numberTwo;
+            return result; 
+        }
+    }
+}
