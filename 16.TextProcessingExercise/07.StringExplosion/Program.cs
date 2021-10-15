be0c23fdfee8a53808b136ec19ff7f6aaa56dd2f
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-15 13:33:27 +0100

    Add files via upload

---
+++ 16.TextProcessingExercise/07.StringExplosion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace _07.StringExplosionSanya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int power = 0;
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char current = input[i];
+
+                if (current =='>')
+                {
+                    power += int.Parse(input[i+1].ToString());
+                    sb.Append(current);
+                }
+                else if (power == 0)
+                {
+                    sb.Append(current);
+                }
+                else
+                {
+                    power--;
+                }
+
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
