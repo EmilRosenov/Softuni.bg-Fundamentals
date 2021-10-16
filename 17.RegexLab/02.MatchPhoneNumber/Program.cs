ab990f85c31636e4c39fc4e9617f8a5f2dcb1c3d
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-16 19:47:07 +0100

    Add files via upload

---
+++ 17.RegexLab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = @"( ?\+359 2 \d{3} \d{4}\b)|(\+ ?359-2-\d{3}-\d{4}\b)";
+
+            var phones = Console.ReadLine();
+
+            
+            var phoneMatches = Regex.Matches(phones,regex);
+
+            var machedPhones = phoneMatches.Cast< Match > ()
+                                           .Select(x => x.Value.Trim())
+                                           .ToArray();
+            Console.WriteLine(string.Join(", ", machedPhones));
+        }
+    }
+}
