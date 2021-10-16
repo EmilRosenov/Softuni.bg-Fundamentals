efea7c122af1a9affc90c59a0f27b571817ff306
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-16 19:45:51 +0100

    Add files via upload

---
+++ 17.RegexLab/01.MatchFullName/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Regex_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<firstName>[A-Z]{1}[a-z]+)\b(?<delimiter>\s)\b(?<secondName>[A-Z]{1}[a-z]+)\b";
+
+            string input = Console.ReadLine();//.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            var matches = Regex.Matches(input, pattern);
+
+            List<string> names = new List<string>();
+
+            foreach (Match item in matches)
+            {
+                names.Add(item.Value);
+            }
+           
+
+            Console.Write(string.Join(" ", names));
+        }
+    }
+}
