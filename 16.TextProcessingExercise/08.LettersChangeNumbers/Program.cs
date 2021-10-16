54743085c6267b693e845044905b1006d4408134
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-16 19:38:48 +0100

    Add files via upload

---
+++ 16.TextProcessingExercise/08.LettersChangeNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _08.LettersChangeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            double result = 0;
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string current = input[i];
+
+                char firstLetter = current[0];
+                char secondLetter = current[current.Length-1];
+
+                double number = double.Parse(current.Substring(1,current.Length-2));
+
+                int indexOfFirstLetter = alphabet.IndexOf(char.ToUpper(firstLetter));
+                int indexOfSecondLetter = alphabet.IndexOf(char.ToUpper(secondLetter));
+
+
+                if (firstLetter >= 65 && firstLetter <=90)
+                {
+                    number = number / (indexOfFirstLetter + 1);
+
+                }
+                else
+                {
+                    number = number * (indexOfFirstLetter + 1);
+                }
+
+                if (secondLetter >= 65 && secondLetter <= 90)
+                {
+                    number = number - (indexOfSecondLetter + 1);
+                }
+                else
+                {
+                    number = number + (indexOfSecondLetter + 1);
+                }
+
+                result += number;
+            }
+
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
