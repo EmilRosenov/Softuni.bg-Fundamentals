7adcd74a7ac1c79d064c85b7d0a0f5948c6fad15
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-17 15:49:21 +0100

    Add files via upload

---
+++ 18.RegexExercise/06.ExtractEmails/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _06.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-][A-Za-z]+)+))(\b|(?=\s))";
+
+            string input = Console.ReadLine();
+            List<string> emailsList = new List<string>();
+
+            var matches = Regex.Matches(input, pattern);
+
+            foreach (Match email in matches)
+            {
+                emailsList.Add(email.Value);
+            }
+
+            foreach (var item in emailsList)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, item));
+            }
+        }
+    }
+}
