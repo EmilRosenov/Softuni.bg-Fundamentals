ee3d7cf7fed45421563266cbba92bde639f7d734
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-18 23:15:33 +0100

    Add files via upload

---
+++ 19.Exams/MidExamsFundamentals/02.ProgFundMidExam/01.SoftuniReception/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _02.ProgrammingFundamentalsMidExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firtsPersonEfficiencyPH = double.Parse(Console.ReadLine());
+            double secondPersonEfficiencyPH = double.Parse(Console.ReadLine());
+            double thirdPersonEfficiencyPH = double.Parse(Console.ReadLine());
+            double peopleCount = double.Parse(Console.ReadLine());
+
+
+
+            double totalEmployeesEfficiencyPH = 0;
+            double timeNeeded = 0;
+            int hours = 0;
+
+
+            while (peopleCount > 0)
+            {
+                hours++;
+                
+                    totalEmployeesEfficiencyPH = firtsPersonEfficiencyPH
+                                           + secondPersonEfficiencyPH
+                                           + thirdPersonEfficiencyPH;
+
+                    peopleCount -= totalEmployeesEfficiencyPH;
+
+                    if (hours % 4 == 0)
+                    {
+                        hours += 1;
+                    }
+
+
+                    timeNeeded = hours;
+
+                    if (peopleCount <= 0)
+                    {
+                        break;
+                    }
+                
+            }
+
+
+            Console.WriteLine($"Time needed: {Math.Round(timeNeeded)}h.");
+        }
+    }
+}
+        
