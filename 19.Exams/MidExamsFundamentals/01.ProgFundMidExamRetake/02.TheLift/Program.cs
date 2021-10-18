6194664f48cf7b1dfa1b4985c289d9ca7b65051b
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-18 23:10:53 +0100

    Add files via upload

---
+++ 19.Exams/MidExamsFundamentals/01.ProgFundMidExamRetake/02.TheLift/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._The_Lift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+
+            int[] lift = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int peopleOnTheCurrentWagon = 0;
+            int peopleonTheLyft = 0;
+
+            bool empty = false;
+
+            for (int i = 0; i < lift.Length; i++)
+            {
+                while (lift[i] < 4)
+                {
+                    lift[i]++;
+                    peopleOnTheCurrentWagon++;
+                    if (peopleonTheLyft + peopleOnTheCurrentWagon == people)
+                    {
+                        empty = true;
+                        break;
+                    }
+                }
+                peopleonTheLyft += peopleOnTheCurrentWagon;
+                if (empty)
+                {
+                    break;
+                }
+                peopleOnTheCurrentWagon = 0;
+            }
+
+
+
+            if (people > peopleonTheLyft)
+            {
+                Console.WriteLine($"There isn't enough space! {people - peopleonTheLyft} people in a queue!");
+                Console.WriteLine(string.Join(" ", lift));
+            }
+            else if (people < lift.Length * 4 && lift.Any(w => w < 4))
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", lift));
+            }
+            else if (lift.All(w => w == 4) && empty == true)
+            {
+                Console.WriteLine(string.Join(" ", lift));
+            }
+        }
+    }
+}
