09aef6354a85d8421260ac7121e79c1b6b48f1cb
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-10-23 13:37:18 +0100

    Add files via upload

---
+++ 19.Exams/MidExamsFundamentals/05.ProgFundMidExam/01.BonusScoringSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Bonus_Scoring_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double countStudents = double.Parse(Console.ReadLine());
+            double countLectures = double.Parse(Console.ReadLine());
+            double initialBonus = double.Parse(Console.ReadLine());
+
+            double biggerNum = 0d;
+            double maxBonus = 0d;
+
+            for (int i = 0; i < countStudents; i++)
+            {
+                double num = double.Parse(Console.ReadLine());
+                double bonus = Math.Ceiling((num / countLectures) * (5d + initialBonus));
+
+                if (num > biggerNum)
+                {
+                    biggerNum = num;
+                    maxBonus = bonus;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {maxBonus:f0}.");
+            Console.WriteLine($"The student has attended {biggerNum:f0} lectures.");
+        }
+    }
+}
